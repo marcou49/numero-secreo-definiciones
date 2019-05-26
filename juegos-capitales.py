3e5d4aa7d1eb131a6bfbcda701255c3a38266be0
marcou49 <richard.casares@gmail.com>
2019-05-27 00:18:04 +0200

    Añadiendo comentarios y ajustando posible respuesta de seguir o no

--- juegos-capitales.py
+++ juegos-capitales.py
@@ -3,23 +3,28 @@ def capitales():
     aciertos = 0
     errores = 0
 
+#creamos diccionario de paises:capitales
     capitales = {'Francia': 'Paris', 'Bielorusia': 'Minsk', 'Malta': 'La Valeta', 'Suiza': 'Berna' }
 
     for pareja in capitales.keys():
         pais = input('Cúal es la capital de '+ pareja +' : ')
 
-        if pais.upper() == capitales[pareja].upper():
+        if pais.upper() == capitales[pareja].upper(): #el usuario acierta sumamos un acierto
             print('Bene')
             aciertos += 1
 
         else:
             print('Niente')
             seguimos = input('Mira que era sencillo,  quiéres seguir S/N: ')
-            errores += 1
+            errores += 1 #error del usuario sumamos un error y le preguntamos si quier seguir
 
             if seguimos.upper() == 'N':
                 print('Mejor dejarlo si')
                 break
+
+            elif seguimos.upper() != 'Y':
+                print('No te entiendo, pero sigamos')
+
 
     print('\nAcertaste: ', aciertos)
     print("Fallaste:", errores)
